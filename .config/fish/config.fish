4a4a890d14babb51b5bb63f380bfcb92edd193a4
Al Colmenar <aljcolmenar@gmail.com>
2024-03-05 16:56:19 -0800

    fix: issue with base16 colorscheme and borders, add scala lsp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,3 +70,11 @@ zoxide init fish | source
 status --is-interactive; and rbenv init - fish | source
 
 alias cd=z
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/Users/alcolmenar/Library/Application Support/Coursier/bin"
+# <<< coursier install directory <<<
+
+# >>> JVM installed by coursier >>>
+set -gx JAVA_HOME "/Users/alcolmenar/Library/Caches/Coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7/Contents/Home"
+# <<< JVM installed by coursier <<<
