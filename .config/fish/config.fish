0561aa444714d490c1d9f297aed03da7bdfd673a
Al Colmenar <aljcolmenar@gmail.com>
2024-06-04 12:02:47 -0700

    remove coursier

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -48,16 +48,10 @@ eval "$(pyenv init -)"
 zoxide init fish | source
 alias cd=z
 
-# >>> coursier install directory >>>
-set -gx PATH "$PATH:/Users/alcolmenar/Library/Application Support/Coursier/bin"
-# <<< coursier install directory <<<
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 direnv hook fish | source
 
-# >>> JVM installed by coursier >>>
-set -gx JAVA_HOME "/Users/alcolmenar/Library/Caches/Coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7/Contents/Home"
-# <<< JVM installed by coursier <<<
 # iex history
 export ERL_AFLAGS="-kernel shell_history enabled"
 
