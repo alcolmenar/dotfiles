130dad3eeeaf17f03d9f1c89730e7cc20fa5c640
Al Colmenar <aljcolmenar@gmail.com>
2024-06-04 12:02:46 -0700

    explicitly set XDG_CONFIG_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,3 +82,4 @@ direnv hook fish | source
 # >>> JVM installed by coursier >>>
 set -gx JAVA_HOME "/Users/alcolmenar/Library/Caches/Coursier/arc/https/github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.22%252B7/OpenJDK11U-jdk_x64_mac_hotspot_11.0.22_7.tar.gz/jdk-11.0.22+7/Contents/Home"
 # <<< JVM installed by coursier <<<
+export XDG_CONFIG_HOME="$HOME/.config"
