09e6bc994aad3a4f9ab6e392bbc654e2f7a7e195
Al Colmenar <aljcolmenar@gmail.com>
2024-06-07 13:15:05 -0700

    feat(nvim): add octo

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,16 @@
+return {
+  "pwntester/octo.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    -- OR 'ibhagwan/fzf-lua',
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("octo").setup({ enable_builtin = true })
+  end,
+
+  keys = {
+    { "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
+  },
+}
