50937691d644739d9657d274213e50c09c48f9d3
Al Colmenar <aljcolmenar@gmail.com>
2024-06-27 13:52:43 -0700

    feat(nvim): allow neotest-attach to be closed by q

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("FileType", {
+  group = "lazyvim_close_with_q",
+  pattern = {
+    "neotest-attach",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
