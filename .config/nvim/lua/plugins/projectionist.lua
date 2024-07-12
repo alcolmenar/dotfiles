c0e5ed4d8c453fc9d9bd449e60fae5ae9ceba16b
Al Colmenar <aljcolmenar@gmail.com>
2024-07-11 17:02:21 -0700

    feat(nvim): add projectionist and elixir

---
+++ .config/nvim/lua/plugins/projectionist.lua
@@ -0,0 +1,12 @@
+return {
+  "andyl/vim-projectionist-elixir",
+  dependencies = {
+    {
+      "tpope/vim-projectionist",
+      keys = {
+        { "<leader>a", "<Cmd>A<CR>", desc = "Alternate File" },
+      },
+    },
+    { "c-brenn/fuzzy-projectionist.vim" },
+  },
+}
