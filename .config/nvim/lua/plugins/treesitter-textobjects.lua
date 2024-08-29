18f309b8dfaa5967d875c465b8cbed62f480d608
Al Colmenar <aljcolmenar@gmail.com>
2024-08-29 11:55:12 -0700

    feat(nvim): remove need for own textobjects

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
-  { "alcolmenar/nvim-treesitter-textobjects", branch = "elixir-fix-functions-guards", enabled = true },
-}
